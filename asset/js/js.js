5bb8f51b4b64df313bdfc89c78a08c1dffa97609
QuangTran <trantran30102002@gmail.com>
2021-09-13 01:40:05 +0700

    fix image slide

--- asset/js/js.js
+++ asset/js/js.js
@@ -21,3 +21,36 @@ model.addEventListener('click',hidenBuy)
 modelContainer.addEventListener('click',function(Event) {
     Event.stopPropagation()
 })
+
+//dong mow menu
+const header = document.getElementById("header")
+const bar = document.querySelector('.js-bar')
+const headerHeight = header.clientHeight
+
+bar.onclick = function() {
+    var isClose = header.clientHeight === headerHeight;
+    if (isClose) {
+        header.style.height = 'auto';
+    }
+    else {
+        header.style.height = null;
+    }
+}
+
+// tu dong dong menu
+const menuIterms = document.querySelectorAll('#nav li a[href*="#"')
+
+for (var i =0; i< menuIterms.length; i++) {
+    var menuIterm = menuIterms[i];
+    
+    menuIterm.onclick = function(event) {
+        var isParent = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav')
+        if (isParent) {
+            event.preventDefault();
+        }
+        else {
+            header.style.height = null;
+        }
+    }
+}
+// khong dong menu khi chon more
